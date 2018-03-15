bff8d0162fce9093189a8c7b572af8d7ff87e014
Gregory Giuliani <gregory.giuliani@unige.ch>
2018-03-15 14:31:08 +0100

    Select specific attributes

--- shp2csv.py
+++ shp2csv.py
@@ -3,4 +3,4 @@ shp_in = "/Users/greg/Desktop/output/kba_ch.shp"
 csv_out = "/Users/greg/Desktop/output/out.csv"
 ogrPath = "/Library/Frameworks/GDAL.framework/Programs/"
 
-os.system(ogrPath+'ogr2ogr -f "CSV" '+csv_out+' '+shp_in)
+os.system(ogrPath+'ogr2ogr -f "CSV" -select "SitRecID,area" '+csv_out+' '+shp_in)
